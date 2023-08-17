2feaf6b9a003ef9c688910455aa1cf1e6faf2099
alex-fanaru <alexfanaru@outlook.com>
2023-08-17 11:01:11 +0100

    Update
    Update

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu(){
+    var menu = document.querySelector(".mobile-menu-links")
+    var icon = document.querySelector(".hamburger-icon")
+    menu.classList.toggle('open')
+    icon.classList.toggle('open')
+}
